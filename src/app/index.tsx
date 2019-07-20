56a71a645e8b1f9549211b1e58d8825e6d25dd26
StireArLankar <stire.ru@gmail.com>
2019-07-20 17:35:08 +0300

    added types for different products

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Context, { IItem } from 'Src/context'
 import Main from 'Src/pages/main'
 import style from './app.module.scss'
 
 const App = () => {
   const [ items, setItems ] = useState<IItem[] | []>([])
+  const [ filteredItems, setFilteredItems ] = useState<IItem[] | []>([])
+
+  const fetchItems = async () => {
+    const url = 'http://avito.dump.academy/products'
+    const { data } = await fetch(url).then((res) => res.json())
+    return data
+  }
+
+  useEffect(() => {
+    const getItems = async () => {
+      const loadedItems = await fetchItems()
+      setItems(loadedItems)
+      setFilteredItems(loadedItems)
+    }
+
+    getItems()
+  }, [])
 
   return (
-    <Context.Provider value={ { temp: items } }>
+    <Context.Provider value={ { items: filteredItems } }>
       <main className={ style.wrapper }>
         <Main />
       </main>
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,15 +1,53 @@
 import React from 'react'
 
-export interface IItem {
-  temp: number
+type Base = {
+  address: {
+    lat: number
+    lng: number
+  }
+  category: string
+  id: string
+  pictures: string[]
+  price: number
+  relationships: {
+    seller: string
+  }
+  title: string
 }
 
+interface IAuto extends Base {
+  body_type: string
+  gearbox: string
+  year: number
+}
+
+interface ILaptop extends Base {
+  laptop_type: string
+  processor: string
+  ram: string
+  screen: string
+}
+
+interface ICamera extends Base {
+  camera_type: string
+  matrix_resolution: number
+  video_resolution: string
+}
+
+interface IImmovable extends Base {
+  property_type: string
+  rooms: number
+  square: number
+}
+
+export type IItem = IAuto | ILaptop | ICamera | IImmovable
+
 interface IContext {
-  temp: IItem[]
+  items: IItem[]
 }
 
 const BookContext = React.createContext<IContext>({
-  temp: [{ temp: 0 }]
+  items: []
 })
 
 export default BookContext
