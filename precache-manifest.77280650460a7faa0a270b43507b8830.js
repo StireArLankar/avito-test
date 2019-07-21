74fad4edc73102beb671d3e958f3a9a9eed75def
StireArLankar <stire.ru@gmail.com>
2019-07-21 19:59:54 +0300

    Updates

---
+++ precache-manifest.77280650460a7faa0a270b43507b8830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399cfc2618a6fd03cd34add13bc6eae6",
+    "url": "/avito-test/index.html"
+  },
+  {
+    "revision": "6846dc1eebef7c5c34d6",
+    "url": "/avito-test/static/css/main.24e3b1a0.chunk.css"
+  },
+  {
+    "revision": "37d19e44fce8e180a34b",
+    "url": "/avito-test/static/js/2.88570b3b.chunk.js"
+  },
+  {
+    "revision": "6846dc1eebef7c5c34d6",
+    "url": "/avito-test/static/js/main.ec3da4f2.chunk.js"
+  },
+  {
+    "revision": "8b2041db9884865d8601",
+    "url": "/avito-test/static/js/runtime~main.679f9f6c.js"
+  },
+  {
+    "revision": "4e2b41a938e1a9d138ce08998a356c37",
+    "url": "/avito-test/static/media/heart.4e2b41a9.svg"
+  }
+]);
